165cf27c2a7a3642378e110c42b59917c86a21a5
Alexandrina Mehandzhiyska <alexandrina.mehandzhiyska1@gmail.com>
2022-06-22 17:31:12 +0300

    Initial commit

---
+++ views/layout/footer.php
@@ -0,0 +1,90 @@
+<!-- Footer -->
+<footer class="text-center text-lg-start bg-primary text-white">
+  <!-- Section: Social media -->
+    <!-- Right -->
+    <div>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-google"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-linkedin"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-github"></i>
+      </a>
+    </div>
+    <!-- Right -->
+  </section>
+  <!-- Section: Social media -->
+
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            <i class="fas fa-gem me-3"></i>Digital Bookstore
+          </h6>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex eos eaque, hic minima sint quaerat magnam sapiente, veritatis cumque architecto libero laudantium dolor commodi ab accusamus quia consequatur aspernatur natus.
+          </p>
+        </div>
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Useful links
+          </h6>
+          <p>
+            <a href="/" class="text-reset">Home</a>
+          </p>
+          <p>
+            <a href="/books" class="text-reset">Books</a>
+          </p>
+          <p>
+            <a href="/about-us" class="text-reset">About Us</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Contacts
+          </h6>
+          <p><i class="fas fa-home me-3"></i> Blagoevgrad, Bulgaria</p>
+          <p>
+            <i class="fas fa-envelope me-3"></i>
+            alex@abv.bg
+          </p>
+          <p><i class="fas fa-phone me-3"></i> 0878787878</p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2021 Copyright:
+    <a class="text-reset fw-bold">Digital Bookstore</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
---
+++ views/layout/header.php
@@ -0,0 +1,21 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary text-white">
+        <div class="container-fluid">
+            <a class="navbar-brand mx-5" href="/">Digital Bookstore</a>
+
+            <ul class="navbar-nav mx-5 me-flex mb-2 mb-lg-0">
+                <li class="nav-item mx-3">
+                    <a class="nav-link active" aria-current="page" href="/">Home</a>
+                </li>
+
+                <li class="nav-item mx-3">
+                    <a class="nav-link" href="/login">Login</a>
+                </li>
+
+                <li class="nav-item mx-3">
+                    <a class="nav-link" href="/register">Register</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
